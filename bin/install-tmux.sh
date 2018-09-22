da5e6213c5785982b66e1a69b43f2d324a8f1853
Kevin Collette <kevcollette@gmail.com>
2018-09-22 01:05:11 -0600

    Make sure in proper directory

--- bin/install-tmux.sh
+++ bin/install-tmux.sh
@@ -1,20 +1,19 @@
 #!/bin/bash -ex
 
 install_libevent() {
-  wget https://github.com/libevent/libevent/releases/download/release-2.0.22-stable/libevent-2.0.22-stable.tar.gz
-  tar xvf libevent-2.0.22-stable.tar.gz
-  cd libevent-2.0.22-stable
-  ./configure --prefix=$HOME/.warp
-  make
-  make install
+  curl -L https://github.com/libevent/libevent/releases/download/release-2.0.22-stable/libevent-2.0.22-stable.tar.gz -o ~/.warp/libevent-2.0.22-stable.tar.gz
+  cd ~/.warp && tar xvf libevent-2.0.22-stable.tar.gz
+  cd ~/.warp/libevent-2.0.22-stable && ./configure --prefix=$HOME/.warp
+  cd ~/.warp/libevent-2.0.22-stable && make
+  cd ~/.warp/libevent-2.0.22-stable && make install
 }
 
 install_tmux() {
-  wget https://github.com/tmux/tmux/releases/download/2.2/tmux-2.2.tar.gz
-  tar xvf tmux-2.2
-  ./configure --prefix=$HOME/.warp CFLAGS="-I$HOME/.warp/include" LDFLAGS="-L$HOME/.warp/lib"
-  make
-  make install
+  curl -L https://github.com/tmux/tmux/releases/download/2.2/tmux-2.2.tar.gz -o ~/.warp/tmux-2.2.tar.gz
+  cd ~/.warp && tar xvf tmux-2.2
+  cd ~/.warp/tmux-2.2 && ./configure --prefix=$HOME/.warp CFLAGS="-I$HOME/.warp/include" LDFLAGS="-L$HOME/.warp/lib"
+  cd ~/.warp/tmux-2.2 && make
+  cd ~/.warp/tmux-2.2 && make install
 }
 
 main() {
